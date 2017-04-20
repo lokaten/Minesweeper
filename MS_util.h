4d37e60c24813512aa237cee006e847cf45fe513
Erik Narstrom <erik.narstrom@gmail.com>
2017-04-20 23:48:54 +0000

    make MS_Free() return NULL, for patern: "ptr \= MS_Free\( ptr\)"

--- MS_util.h
+++ MS_util.h
@@ -99,7 +99,7 @@ typedef struct{
 #ifdef LOCALE_
 
 INLINE void *LOCALE_( MS_Create)( size_t, void *);
-INLINE void LOCALE_( MS_Free)( void *);
+INLINE void *LOCALE_( MS_Free)( void *);
 INLINE unsigned long LOCALE_( gen_divobj)( unsigned long);
 INLINE unsigned long LOCALE_( mol_)( unsigned long, unsigned long, unsigned long);
 INLINE unsigned long LOCALE_( div_)( unsigned long, unsigned long, unsigned long);
@@ -122,9 +122,10 @@ LOCALE_( MS_Create)( size_t alo_size, void *data){
 #define MS_Create( type, exp) ( type *)LOCALE_( MS_Create)( sizeof( type), ( void *)&( exp))
 #define MS_CreateEmpty( type) ( type *)LOCALE_( MS_Create)( sizeof( type), ( void *)&( ( type){0}))
 
-INLINE void
+INLINE void *
 LOCALE_( MS_Free)( void *ptr){
   if( ptr != NULL) free( ptr);
+  return NULL;
 }
 #define MS_Free( exp) LOCALE_( MS_Free)( ( void *)(exp))
 
