eb1e6062db193e207d2080bd15ea31814909f3cc
Erik Narstrom <erik.narstrom@gmail.com>
2017-04-21 19:41:37 +0000

    give us handy tools for beter intgers

--- MS_util.h
+++ MS_util.h
@@ -49,9 +49,19 @@ extern "C" {
 #define assert( exp) ( void)( exp);
 #endif
 
+typedef  uint_fast64_t u64;
+typedef  uint_fast32_t u32;
+typedef  uint_fast16_t u16;
+typedef  uint_fast8_t  u8;
+
+typedef  int_fast64_t s64;
+typedef  int_fast32_t s32;
+typedef  int_fast16_t s16;
+typedef  int_fast8_t  s8;
+
 typedef struct{
-  unsigned long x;
-  unsigned long y;
+  u16 x;
+  u16 y;
 }MS_pos;
 
     
