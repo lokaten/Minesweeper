9e1df4f6b488dac85d00c5daada1260b84c089bb
EKG <ekg@librem.one>
2019-09-29 19:06:48 +0200

    refactor OPT.h (part 2)

--- OPT.h
+++ OPT.h
@@ -24,15 +24,16 @@ typedef struct{
 }MS_options;
 
 
-int arg_too_ul( char *, void *);
-int arg_too_x( char *, void *);
+int arg_too_ul( char **, void *);
+int arg_too_x( char **, void *);
+int arg_too_cpy( char **, void *);
 
 
 #define OPTSW_NUL 0
 #define OPTSW_LU  ( uintptr_t)&arg_too_ul
 #define OPTSW_X   ( uintptr_t)&arg_too_x
 #define OPTSW_GRP 4
-#define OPTSW_CPY 5
+#define OPTSW_CPY ( uintptr_t)&arg_too_cpy
 #define OPTSW_RAW 6
 
 #define OPT_MAX 200
@@ -41,11 +42,11 @@ static inline int procopt( const MS_stream *, const MS_options *, const int, con
 static inline int help( FILE *, const MS_options *);
 
 int
-arg_too_ul( char *arg, void *popt){
-  int ret = 0;
+arg_too_ul( char **arg, void *popt){
+  int ret = 1;
   char *lr = NULL;
   MS_options opt = *(MS_options *)popt;
-  *( unsigned long *)( opt.data) = strtoul( arg, &lr, 10);
+  *( unsigned long *)( opt.data) = strtoul( arg[ ret], &lr, 10);
   if( *lr != 0 || ( errno != 0)){
     ret = -1;
   }
@@ -53,14 +54,23 @@ arg_too_ul( char *arg, void *popt){
 }
 
 int
-arg_too_x( char *arg, void *popt){
-  int ret = 0;
+arg_too_x( char **arg, void *popt){
+  int ret = 1;
   char *lr = NULL;
   MS_options opt = *(MS_options *)popt;
-  *( unsigned long *)( opt.data) = strtoul( arg, &lr, 10);
+  *( unsigned long *)( opt.data) = strtoul( arg[ ret], &lr, 10);
   if( *lr != 0 || ( errno != 0)){
     ret = -1;
   }
+  return ret;
+}
+
+int
+arg_too_cpy( char **arg, void *popt){
+  int ret = 0;
+  MS_options opt = *(MS_options *)popt;
+  ( void)arg;
+  *( const void **)( opt.data) = ( const void *)( opt.value);
   return ret;
 }
 
@@ -76,12 +86,12 @@ procopt( const MS_stream *mss, const MS_options *opt, const int argc, const char
         if( ( uintptr_t)opt[ j].func){
           if( strcmp( argv[ i] + 2, opt[ j].name) == 0){
             errno = 0;
-	    if( ( uintptr_t)opt[ j].func == OPTSW_CPY){
-	      *( const void **)( opt[ j].data) = ( const void *)( opt[ j].value);
-	    }else{
-	      if( ++i < argc){
-		int ( *func)( const char *, const void *) = ( int( *)( const char *, const void *))opt[j].func;
-		ret = func( argv[ i], ( const void *)&opt[ j]);
+	    {
+	      int ( *func)( const char **, const void *) = ( int( *)( const char **, const void *))opt[j].func;
+	      ret = func( &argv[ i], ( const void *)&opt[ j]);
+	      if( ret > 0){
+		i += ret;
+		ret = 0;
 	      }
 	    }
             break;
@@ -104,15 +114,15 @@ procopt( const MS_stream *mss, const MS_options *opt, const int argc, const char
             if( *( argv[ i] + k) &&
                 ( *( argv[ i] + k) == opt[ j].chr)){
               errno = 0;
-	      if( ( uintptr_t)opt[ j].func == OPTSW_CPY){
-		*( const void **)( opt[ j].data) = ( const void *)( opt[ j].value);
-              }else{
-		if( ++i < argc){
-		  int ( *func)( const char *, const void *) = ( int( *)( const char *, const void *))opt[j].func;
-		  ret = func( argv[ i], ( const void *)&opt[ j]);
+	      {
+		int ( *func)( const char **, const void *) = ( int( *)( const char **, const void *))opt[j].func;
+		ret = func( &argv[ i], ( const void *)&opt[ j]);
+		if( ret > 0){
+		  i += ret;
+		  ret = 0;
 		}
-              }
-              break;
+	      }
+	      break;
 	    }
           }else{
             MS_print( mss -> err, "\rShort option \'%c\' dosen't exist\n", *( argv[ i] + k));
